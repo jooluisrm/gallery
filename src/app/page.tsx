b4ca2e1e5fc5ea28cc98c77bd77d234431de620f
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-11 15:45:08 -0300

    pagina app com styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 const App = () => {
     return (
-        <div className="container m-auto">
-            <div>
+        <div className="bg-[#27282F] text-white min-h-screen">
+            <div className="container m-auto max-w-[980px] py-8 px-0">
                 <header>
-                    <h1>Galeria de Fotos</h1>
+                    <h1 className="m-0 p-0 text-center mb-8 text-3xl font-bold">Galeria de Fotos</h1>
                 </header>
 
                 {/* Area de upload */}
