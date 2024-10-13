548ecc4b9b8497970ab31d28f400477e4f1f3521
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-13 17:17:05 -0300

    Configurando .env

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAV7q5Zkt-zBArcUl9Y0S3p0_4gwoV_VRY",
-    authDomain: "d5reactgallery-2575f.firebaseapp.com",
-    projectId: "d5reactgallery-2575f",
+    apiKey: process.env.NEXT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_APP_FIREBASE_PROJECTID,
     storageBucket: "d5reactgallery-2575f.appspot.com",
-    messagingSenderId: "623758959056",
-    appId: "1:623758959056:web:3e8a67735cc35e56d659d5"
+    messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSEAGINGSENDERID,
+    appId: process.env.NEXT_APP_FIREBASE_APPID
 };
 
 console.log("API Key: ", process.env.NEXT_APP_FIREBASE_APIKEY);
